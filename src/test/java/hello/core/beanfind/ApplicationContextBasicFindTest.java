dfd7caf36128dc35c178223d7571539d0ff025cc
kim <youoper@gmail.com>
2020-11-24 22:40:57 +0900

    find all bean test added

--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -7,7 +7,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -42,6 +45,23 @@ class ApplicationContextBasicFindTest {
 //        MemberService xxx = ac.getBean("xxx", MemberServiceImpl.class);
         assertThrows(NoSuchBeanDefinitionException.class,
                 () -> ac.getBean("xxx", MemberServiceImpl.class));
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 조회, 자식이 둘 이상 있으면, 중복 오류")
+    void findBeanByParentTypeDuplicate() {
+        assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> ac.getBean(Object.class));
+    }
+
+
+    @Test
+    @DisplayName("부모 타입으로 조회, 여러 빈을 모두 조회")
+    void findAllBeanByParentType() {
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
     }
 
 }
